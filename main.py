845def736845c6510d986ed25884bfbf5cb77744
三浦だよ <sab01156@edu.osakafu-u.ac.jp>
2019-12-25 20:17:13 +0900

    change

--- main.py
+++ main.py
@@ -1,51 +1,43 @@
 from flask import Flask, request, abort
 
 from linebot import (
-    LineBotApi, WebhookHandler
+ LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+ InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+ MessageEvent, TextMessage, TextSendMessage,
 )
-import os
 
 app = Flask(__name__)
 
-#環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
-
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+# 自分のLINE botのデータを設定する。
+line_bot_api = LineBotApi('アクセストークン')
+handler = WebhookHandler('Channel Secret')
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+ # get X-Line-Signature header value
+ signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+# get request body as text
+ body = request.get_data(as_text=True)
+ app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+# handle webhook body
+ try:
+  handler.handle(body, signature)
+ except InvalidSignatureError:
+  abort(400)
 
-    return 'OK'
-
+ return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-
+ line_bot_api.reply_message(
+  event.reply_token,
+  TextSendMessage(text=event.message.text))
 
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+ app.run()
