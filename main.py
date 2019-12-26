b7826bc0c2bef56b32d733f1ec78393575ccb889
三浦だよ <sab01156@edu.osakafu-u.ac.jp>
2019-12-26 21:24:54 +0900

    Improve

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-import config, lineapphandl, richmenu,reply
 import os
-
 
 app = Flask(__name__)
 
@@ -20,7 +18,6 @@ YOUR_CHANNEL_ACCESS_TOKEN = config.YOUR_CHANNEL_ACCESS_TOKEN
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -35,7 +32,6 @@ def callback():
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
@@ -43,15 +39,12 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    lineapphandl.TextMessage(event)
-
-# check for existing richmenu
-rich_menu_list = line_bot_api.get_rich_menu_list()
-if not rich_menu_list:
-    result = richmenu.createRichmenu(line_bot_api)
-    if not result:
-        reply.reply_message(event, config.FAILED)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+#    app.run()
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=port)
