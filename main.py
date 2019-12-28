c51f45ae0011e752f92b8105fc99e5f747881c97
三浦だよ <sab01156@edu.osakafu-u.ac.jp>
2019-12-28 16:18:06 +0900

    Improve

--- main.py
+++ main.py
@@ -81,10 +81,16 @@ def handle_message(event):
         return
     
     text=event.message.text
-    if text == 'あ':
-        confirm_template = ConfirmTemplate(text='Do it?', actions=[
-            MessageAction(label='Yes', text='Yes!'),
-            MessageAction(label='No', text='No!'),
+    if text == 'land':
+        confirm_template = ConfirmTemplate(text='エリアを選択してください', actions=[
+            MessageAction(label='ワールドバザール', text='ワールドバザール'),
+            MessageAction(label='アドベンチャー', text='アドベンチャー'),
+            MessageAction(label='ウエスタン', text='ウエスタン'),
+            MessageAction(label='クリッター', text='クリッター'),
+            MessageAction(label='ファンタジー', text='ファンタジー'),
+            MessageAction(label='トゥーン', text='トゥーン'),
+            MessageAction(label='トゥモロー', text='トゥモロー'),
+
         ])
         template_message = TemplateSendMessage(
             alt_text='Confirm alt text', template=confirm_template)
