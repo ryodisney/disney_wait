b6ba3b158b2d61156d91d5b840fb3f9cc61dfecf
三浦だよ <sab01156@edu.osakafu-u.ac.jp>
2019-12-28 16:49:17 +0900

    Improve

--- profile.py
+++ profile.py
@@ -1,6 +1,6 @@
+from flask import Flask, request, abort
 import os
 
-from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -8,25 +8,32 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, MessageAction, TemplateSendMessage,
-    ButtonsTemplate)
+    MessageEvent,TextMessage,TextSendMessage,ConfirmTemplate,MessageAction,TemplateSendMessage,QuickReplyButton,QuickReply,RichMenu, RichMenuSize, RichMenuArea, RichMenuBounds,URIAction
+)
 
 app = Flask(__name__)
 
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+#環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+@app.route("/")
+def hello_world():
+    return "hello world!"
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
