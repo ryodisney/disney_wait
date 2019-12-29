d0f7a614c5fb51f78ac7e4281759bb60345466a4
三浦だよ <sab01156@edu.osakafu-u.ac.jp>
2019-12-30 07:29:10 +0900

    Improve

--- main.py
+++ main.py
@@ -92,11 +92,13 @@ def handle_message(event):
 def on_postback(event):
     #reply_token = event.reply_token
     user_id = event.source.user_id
-    postback_msg = event.postback.data       
-
+    postback_msg = event.postback.data
+    
     line_bot_api.push_message(
-        to=user_id,
-        messages=TextSendMessage(text=postback_msg)
+    to=user_id,
+    messages=TextSendMessage(text="aaa")       
+
+
     )
 
 if __name__ == "__main__":
