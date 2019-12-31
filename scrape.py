4236c73e50580cdce39ad0e84c10a2a9e413063a
三浦だよ <sab01156@edu.osakafu-u.ac.jp>
2020-01-01 06:07:19 +0900

    Improve

--- scrape.py
+++ scrape.py
@@ -56,7 +56,7 @@ def Match_area(attraction_list,info_list,area):
 def Set(park,area):
     options = Options()
     options.set_headless(True)
-    driver_path = "C:/Users/ryo/Desktop/必要なモノ/chromedriver_win32/chromedriver"
+    driver_path = "C:/Users/ryo/Desktop/programm/line_bot/chromedriver"
     options.add_argument("--user-agent=Mozilla/5.0")
     driver = webdriver.Chrome(driver_path,options=options)
 
