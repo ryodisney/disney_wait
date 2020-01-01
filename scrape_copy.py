34a295cfcf803833fb6bb4218c712a8b291bf2fb
三浦だよ <sab01156@edu.osakafu-u.ac.jp>
2020-01-02 08:05:48 +0900

    Improve

--- scrape_copy.py
+++ scrape_copy.py
@@ -114,7 +114,7 @@ def Set(park,area):
     options = webdriver.ChromeOptions()
     options.set_headless(True)
     options.add_argument('--headless')
-    options.add_argument('--user-agent=Mozilla/5.0')
+    options.add_argument('--user-agent=Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)')
     driver = webdriver.Chrome(options=options)
 
     #スクレイピングするサイトのURL
