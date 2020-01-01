584db512207519a65addc107442b4363bc8f3d3a
三浦だよ <sab01156@edu.osakafu-u.ac.jp>
2020-01-02 03:14:24 +0900

    Improve

--- main.py
+++ main.py
@@ -88,6 +88,7 @@ def handle_postback(event):
     les = "les"
     template = template_env.get_template('recipt.json')
     data = template.render(dict(items=les))
+    print(data)
 
     line_bot_api.reply_message(
     event.reply_token,
