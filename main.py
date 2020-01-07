ec5ef914c0bac3893a3d54461c78adf8522b9da7
三浦だよ <sab01156@edu.osakafu-u.ac.jp>
2020-01-08 01:16:26 +0900

    Improve

--- main.py
+++ main.py
@@ -87,11 +87,11 @@ def handle_postback(event):
     line_bot_api.push_message(userid, messages=confirm_message)
 
     #ファイルのコピー、入れ替え
-    src = 'app/recipt.json'
-    scr_template = 'app/recipt_template.json'
+    src = '/app/recipt.json'
+    scr_template = '/app/recipt_template.json'
     if os.path.isfile(src) and os.path.isfile(scr_template):
         print("コピー")
-        shutil.copy('app/recipt_template.json', 'app/recipt.json')
+        shutil.copy('/app/recipt_template.json', '/app/recipt.json')
 
 
     #開閉園、スクレイピング、レシート作成
